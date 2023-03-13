dfa8f8a594d2a599fd1d4ed0142aa8219f81b892
riazul islam <riazulislam5280@gmail.com>
2023-03-13 21:53:05 +0600

    basic project setup completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-400">Hello!</h1>
+    </div>
+  );
+}
+
+export default App;
